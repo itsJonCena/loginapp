ec588ff27b13b50b3391ff353c03a155184006bc
Freddy Alexandro Gamboa Velasco <fredogodofredo615@gmail.com>
2017-08-01 16:28:19 -0500

    app start

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'sql3.freemysqlhosting.net',
+        //'host': '127.0.0.1',
+        //'user': 'userLg',
+        'user': 'sql3188167',
+        //'password': 'admin1234'
+        'password': 'ArL36Mrtux'
+    },
+    'database': 'sql3188167',
+    'users_table': 'Users'
+};
