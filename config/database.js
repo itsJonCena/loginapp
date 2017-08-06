4a344d9b6eb8b90b05c0558845a7bebc23d57f93
Freddy Alexandro Gamboa Velasco <fredogodofredo615@gmail.com>
2017-08-06 14:08:28 -0500

    *** app The open Library *** ->Profile routing ->Insert, Select and Delete action only by Publisher profile ->Select only by Reader profile

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 // config/database.js
 module.exports = {
     'connection': {
-        //'host': 'sql3.freemysqlhosting.net',
+        'host': 'sql3.freemysqlhosting.net',
         //'host': '192.168.208.129',
-        'host': 'localhost',
-        //'user': 'sql3188167',
+        //'host': 'localhost',
+        'user': 'sql3188768',
         //'user': 'userLg',
-        'user': 'root',
-        //'password': 'ArL36Mrtux'
-        'password': 'admin1234'
+        //'user': 'root',
+        'password': 'EWcTC7hBG1'
+        //'password': 'admin1234'
     },
     //'database': 'sql3188167',
-    'database': 'loginapp',
+    'database': 'sql3188768',
     'users_table': 'Users'
 };
